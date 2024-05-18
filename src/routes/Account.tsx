68c7d7d8df92acf9e04d8233d728c6cfeb9f97b7
Sanjeet <thuyatun31@gmail.com>
2024-05-18 23:02:26 +0630

    Front-end and back-end communicate

--- src/routes/Account.tsx
+++ src/routes/Account.tsx
@@ -1,9 +1,38 @@
 import SearchInput from "@/components/custom/SearchInput";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useState } from "react";
+
+interface Account {
+  id: number;
+  name: string;
+  type: string;
+  description: string;
+}
 
 const Account = () => {
+  const [accounts, setAccounts] = useState<Account[]>([]);
+
+  const getAccounts = () => {
+    axios
+      .get<Account[]>("/api/account/getAccounts")
+      .then((resp) => {
+        setAccounts(resp.data);
+
+        console.log(resp.data);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="flex flex-col">
       <SearchInput />
+      <Button onClick={getAccounts}>Get Accounts</Button>
+      <ul>
+        {accounts.map((x) => (
+          <li key={x.id}>{x.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
