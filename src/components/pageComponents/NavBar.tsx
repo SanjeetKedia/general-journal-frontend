4c91ee84ae30cff3b40cf481a9a4ae7253589527
Sanjeet <thuyatun31@gmail.com>
2024-08-27 18:28:43 +0630

    Expandable Navbar

--- src/components/pageComponents/NavBar.tsx
+++ src/components/pageComponents/NavBar.tsx
@@ -1,24 +1,47 @@
 import { Separator } from "@radix-ui/react-separator";
+import { Menu, SidebarCloseIcon, SidebarOpenIcon } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = ({ routes }: { routes: { route: string; text: string }[] }) => {
+  const [isCollasped, setIsCollasped] = useState(true);
+
+  const toggleMenu = () => setIsCollasped(!isCollasped);
+
+  const handleMouseOver = () => setIsCollasped(false);
+  const handleMouseExit = () => setIsCollasped(true);
+
   return (
     <>
-      <nav className="flex flex-col w-1/12 bg-secondary text-2xl pt-2 h-screen text-secondary-foreground">
-        <h1 className="text-center pb-2">Menu</h1>
-        <Separator className="mx-auto h-1 rounded-full" />
-        {routes.map((route, i) => {
-          return (
-            <Link
-              className="p-2 hover:bg-primary hover:text-primary-foreground transition-colors ease-in "
-              to={route.route}
-              key={i}
-            >
-              {route.text}
-            </Link>
-          );
-        })}
-      </nav>
+      {isCollasped ? (
+        <nav className="flex-flex-col pt-5 w-auto transition-all duration-500 ease-in-out h-screen">
+          <SidebarOpenIcon
+            className="cursor-pointer hover:bg-secondary h-full mt-2 mr-2 text-primary"
+            onClick={toggleMenu}
+            onMouseOver={handleMouseOver}
+            onMouseLeave={handleMouseExit}
+          />
+        </nav>
+      ) : (
+        <nav
+          className="flex flex-col min-w-fit bg-secondary text-2xl pt-5 h-screen text-secondary-foreground transition-all duration-500 ease-out"
+          onMouseLeave={handleMouseExit}
+        >
+          <h1 className="text-center pb-2">Menu</h1>
+          <Separator className="mx-auto h-1 rounded-full" />
+          {routes.map((route, i) => {
+            return (
+              <Link
+                className="p-2 hover:bg-primary hover:text-primary-foreground transition-colors ease-in pr-6"
+                to={route.route}
+                key={i}
+              >
+                {route.text}
+              </Link>
+            );
+          })}
+        </nav>
+      )}
     </>
   );
 };
