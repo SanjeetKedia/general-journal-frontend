387ae7e6260205187f0e068acb809f97dd3495c0
Thuya Tun <thuyatun31@gmail.com>
2024-09-18 14:08:23 +0630

    Minor Improve

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
@@ -12,7 +12,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
   server: {
