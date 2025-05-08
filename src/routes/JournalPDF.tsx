95e610ac929bb4236a5b179f96cc571b159bd443
Sanjeet <thuyatun31@gmail.com>
2025-05-08 15:40:43 +0630

    Add Myanmar Font

--- src/routes/JournalPDF.tsx
+++ src/routes/JournalPDF.tsx
@@ -1,12 +1,24 @@
 import { formatMoney } from "@/lib/helpers";
 import { AccountTotals, JournalTransaction } from "@/lib/types";
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Font,
+} from "@react-pdf/renderer";
+
+Font.register({
+  family: "MMRText",
+  src: "/fonts/mmrtext.ttf",
+});
 
 const styles = StyleSheet.create({
   page: {
     padding: 20,
     fontSize: 10,
-    fontFamily: "Helvetica",
+    fontFamily: "MMRText",
     position: "relative",
   },
   title: {
