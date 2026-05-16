984d3d27ebfcc403c4a0cab3a249785b388e0faf
Sanjeet <thuyatun31@gmail.com>
2026-05-16 15:02:57 +0630

    fix: Package Changes

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const apiURL = import.meta.env.VITE_API_URL /*|| "http://localhost:3000"*/;
 
 const apiClient = axios.create({
   baseURL: apiURL,
